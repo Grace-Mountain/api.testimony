67cab929a91a3461885620238240bc7fb903dd45
Rodney Hinson <rodneyhinson22@gmail.com>
2024-12-18 21:42:52 +0000

    js file modified

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import userRouter from './routes/user-router.js';
 import testimonyRouter from './routes/testimony-router.js';
 
 // Connect to database
-// await mongoose.connect(process.env.MONGO_URI)
-//     .then(() => console.log("Database connected successfully"))
-//     .catch((error) => console.log("Error connecting to database", error));
+await mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Database connected successfully"))
+    .catch((error) => console.log("Error connecting to database", error));
 
 // Create an express app
 const app = express();
